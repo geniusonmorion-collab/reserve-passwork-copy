433cddcb3f0774d3bb2b0dcad8f52acfb3276beb
Morion <geniusonmorion@gmail.com>
2026-09-08 23:08:25 +0500

    Replace certification graphic with the Figma gradient

--- app/security-proof-grid.tsx
+++ app/security-proof-grid.tsx
@@ -1,5 +1,4 @@
 import ApplicationSectors from './application-sectors';
-import CertificationMotion from './certification-motion';
 
 /* Figma 517:10784: certification panel beside a two-by-two sector grid. */
 
@@ -11,7 +10,6 @@ export default function SecurityProofGrid() {
           <h3 id="security-proof-fstec">ФСТЭК России, 4 уровень доверия</h3>
           <p>Соответствует требованиям госсектора<br />и критической инфраструктуры</p>
         </div>
-        <CertificationMotion />
       </article>
       <ApplicationSectors />
     </div>
